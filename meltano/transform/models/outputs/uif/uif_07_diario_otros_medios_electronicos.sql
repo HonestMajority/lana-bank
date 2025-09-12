23c0e92c9183912c2a85e639b10f6bee48169cd5
pmartincalvo <pablomartincalvo@gmail.com>
2025-09-12 10:19:20 +0200

    feat: placeholder for uif 07 report output (#2720)
    * feat: add model with placeholders
    
    * fields and docs for uif07
    
    * chore: add more docs
    
    * chore: some docs, some notes
    
    * chore: more accurate docs
    
    * chore: notes
    
    * chore: add exposure entry
    
    * chore: delete notes
    
    * chore: add files to typos ignore list

---
+++ meltano/transform/models/outputs/uif/uif_07_diario_otros_medios_electronicos.sql
@@ -0,0 +1,18 @@
+select
+    null as numeroRegistroBancario,
+    null as estacionServicio,
+    null as fechaTransaccion,
+    null as tipoPersonaA,
+    null as detallesPersonaA,
+    null as tipoPersonaB,
+    null as detallesPersonaB,
+    null as numeroCuentaPO,
+    null as claseCuentaPO,
+    null as conceptoTransaccionPO,
+    null as valorOtrosMediosElectronicosPO,
+    null as numeroProductoPB,
+    null as claseCuentaPB,
+    null as montoTransaccionPB,
+    null as valorMedioElectronicoPB,
+    null as bancoCuentaDestinatariaPB
+
