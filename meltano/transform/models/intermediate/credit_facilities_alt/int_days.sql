a4b150e91d593ecb1e26b38eefe0b11f09dbfacd
José <jir@galoy.io>
2025-10-01 12:01:02 -0600

    ci: run dbt tests in github actions (#2780)
    * ci: run dbt tests in github actions
    
    * chore(reporting): remove unmantained holistics models with failing tests
    
    * chore: turn down severity of latest_values test until the column is fixed
    
    * fix: add accepted_range test macro and update not_null tests to warn
    
    * fix: disable quoting for accepted_values tests in tipoPersona
    
    * fix: use not_null.config.severity for warnings in uif tests

--- meltano/transform/models/intermediate/credit_facilities_alt/int_days.sql
+++
@@ -1,7 +0,0 @@
-select
-    date(requested_at) as day,
-    any_value(last_price_usd having max requested_at) as close_price_usd_per_btc
-
-from {{ ref('stg_bitfinex_ticker_price') }}
-
-group by day
