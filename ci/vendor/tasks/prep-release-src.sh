77cb7c8ba1167a24e92597fdee6ecd8e79f178cd
bodymindarts <justin@galoy.io>
2025-10-22 15:00:11 +0200

    ci: use cocogitto to bump version

--- ci/vendor/tasks/prep-release-src.sh
+++ ci/vendor/tasks/prep-release-src.sh
@@ -29,29 +29,9 @@ echo "-------------------------------"
 cat artifacts/gh-release-notes.md
 echo "-------------------------------"
 
-# ------------ BUMP VERSION ------------
-
-echo -n "Prev Version: "
-cat version/version
-echo ""
-
-# Initial Version
-if [[ $(cat version/version) == "0.0.0" ]]; then
-  echo "0.1.0" > version/version
-# Figure out proper version to release
-elif [[ $(cat artifacts/gh-release-notes.md | grep breaking) != '' ]] || [[ $(cat artifacts/gh-release-notes.md | grep feature) != '' ]]; then
-  echo "Breaking change / Feature Addition found, bumping minor version..."
-  bump2version minor --current-version $(cat version/version) --allow-dirty version/version
-else
-  echo "Only patches and fixes found - no breaking changes, bumping patch version..."
-  bump2version patch --current-version $(cat version/version) --allow-dirty version/version
-fi
-
 echo -n "Release Version: "
 cat version/version
 echo ""
-
-# ------------ ARTIFACTS ------------
 
 cat version/version > artifacts/gh-release-tag
 echo "v$(cat version/version) Release" > artifacts/gh-release-name
