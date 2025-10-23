eb1267081c992424755d4a5d251e411d763687d4
José <jir@galoy.io>
2025-10-23 15:40:23 -0600

    chore(reporting): data tests for nrp-41 reports (#2875)
    * chore: add dbt_utils
    
    * chore: tests and documentation for main reports in nrp41
    
    * test: add relationship tests for report_nrp_41 reference and guarantee models
    
    * test: add relationships test for num_referencia in report_nrp_41_03
    
    * fix: add missing relationships in 02_referencia and 07_garantia_pignorada
    
    * chore: sqlfluff
    
    * chore: remove unused tests
    
    * chore: install deps first
    
    * chore: install dbt deps for sqlfluff workflow
    
    * chore: reduce severity of failing data tests
    
    Can return to default severity after fixes are merged.
    
    * chore: install dbt-bigquery for sqlfluff workflow
    
    * fix: github workflow file format
    
    * chore: increase severity for fixed tests

--- meltano/transform/models/intermediate/el-salvador-ssf/int_nrp_41_07_garantia_pignorada.sql
+++ meltano/transform/models/intermediate/el-salvador-ssf/int_nrp_41_07_garantia_pignorada.sql
@@ -1,20 +1,19 @@
 select
-    -- loan-to-collateral being 1-to-1
     disbursement_public_ids.id as `identificacion_garantia`,
 
     customer_public_ids.id as `nit_depositante`,
 
-    -- Deposit date.
     disbursal_end_date as `fecha_vencimiento`,
 
-    -- Due date of the deposit.
+    -- loan-to-collateral being 1-to-1
     collateral_amount_usd as `valor_deposito`,
-    'DE' as `tipo_deposito`,
 
     -- "DE" for cash deposits
-    'BC99' as `cod_banco`,
+    'DE' as `tipo_deposito`,
 
-    -- "BC99" for a yet undefined lana bank
+    null as `cod_banco`,
+
+    -- Deposit date.
     date(most_recent_collateral_deposit_at) as `fecha_deposito`
 
 from {{ ref('int_approved_credit_facility_loans') }}
